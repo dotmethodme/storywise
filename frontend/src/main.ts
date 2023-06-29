676f8b3c1ebecbbbb6d4df102a287fab4ac6eec9
Mihai Nueleanu <mihai.nueleanu@gmail.com>
2023-06-29 21:47:46 +0200

    feat: using a better font

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,6 +3,7 @@ import "./assets/main.css";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import "./app.css";
+import "@fontsource-variable/inter";
 
 import App from "./App.vue";
 import router from "./router";
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -6,6 +6,9 @@ import Referrers from "@/components/Referrers.vue";
 
 <template>
   <main class="w-full">
+    <h1 class="mb-10 text-2xl font-extrabold tracking-tight text-gray-900">
+      Storywise / <span class="text-gray-500">analytics made simple</span>
+    </h1>
     <SessionsOverTime />
     <div class="mt-10"></div>
     <Sessions />
