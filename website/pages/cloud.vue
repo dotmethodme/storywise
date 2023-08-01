860548e5ccbd18c83deead19e7aee264e1152736
Mihai Nueleanu <mihai.nueleanu@gmail.com>
2023-08-01 20:32:56 +0200

    feat: cloud website page

---
+++ website/pages/cloud.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div
+    class="px-4 py-8 max-w-screen-xl m-auto grid grid-cols-2 justify-center gap-4"
+  >
+    <div class="flex flex-col flex-1 align-center justify-center">
+      <h1 class="serif text-4xl mb-4 font-bold">The storywise cloud</h1>
+      <p class="sans text-xl max-w-lg">
+        A simple and robust architecture where storywise does all the work, but
+        you're in control.
+      </p>
+    </div>
+
+    <div class="max-w-xl">
+      <img
+        src="/assets/images/cloud-infra.png"
+        alt="Architecture of storywise cloud"
+      />
+    </div>
+  </div>
+
+  <Waitlist />
+</template>
