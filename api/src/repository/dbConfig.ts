0f0c9f6d646e606a0c5e5bd52dd92706b3fecd6e
Mihai Nueleanu <mihai.nueleanu@gmail.com>
2023-09-04 23:38:51 +0200

    fix: database name

--- api/src/repository/dbConfig.ts
+++ api/src/repository/dbConfig.ts
@@ -1,3 +1,4 @@
 export const MONGODB_URI = process.env.MONGODB_URI;
 export const POSTGRES_URL = process.env.POSTGRES_URL;
 export const LIBSQL_URL = process.env.LIBSQL_URL;
+export const DATABASE_NAME = process.env.DATABASE_NAME;
