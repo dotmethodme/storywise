90b389a6b434661b265f073c77f1bcad4bdc435b
Mihai Nueleanu <mihai.nueleanu@gmail.com>
2023-09-12 22:43:15 +0200

    started onboarding

--- website/pages/onboarding.vue
+++ website/pages/onboarding.vue
@@ -22,8 +22,24 @@ onMounted(() => {
 });
 </script>
 <template>
-  <div>
-    <h1>Onboarding</h1>
-    <p>This is the onboarding page for variant {{ plan }}.</p>
+  <div class="flex justify-center items-center flex-col max-w-md m-auto mt-20">
+    <h1 class="serif text-3xl mb-4 text-center">Register for storywise cloud</h1>
+    <p class="mb-4">
+      You've chosen the {{ plan?.size }}, billed {{ plan?.type }}, priced at {{ plan?.price }}. First, we'll
+      create a storywise account for you. Then we'll take you through the payment process. Let's get started!
+    </p>
+
+    <div class="form-control">
+      <label class="label cursor-pointer">
+        <input type="checkbox" checked="checked" class="checkbox checkbox-primary mr-4" />
+        <span class="label-text">
+          By creating an account, you accept our
+          <NuxtLink class="link" href="/terms">Terms of Service</NuxtLink> and
+          <NuxtLink class="link" href="/privacy">Privacy Policy</NuxtLink>
+        </span>
+      </label>
+    </div>
+
+    <button class="btn btn-primary mt-4">Register with GitHub</button>
   </div>
 </template>
