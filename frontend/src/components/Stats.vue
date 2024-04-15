117e0e1c511fe07bc0d2875be150727462d7b951
Mihai Nueleanu <mihai.nueleanu@gmail.com>
2024-04-15 10:08:57 +0200

    stats format

--- frontend/src/components/Stats.vue
+++ frontend/src/components/Stats.vue
@@ -18,25 +18,29 @@ async function fetchData(activeAppId: string, days: number) {
   stats.value = result.data.item;
 }
 
+function formatNumber(value: string | null) {
+  return value ? new Intl.NumberFormat().format(parseFloat(value)) : "";
+}
+
 onMounted(() => fetchData(store.activeAppId, store.selectedDays));
 watch([activeAppId, selectedDays], ([activeAppId, selectedDays]) => fetchData(activeAppId, selectedDays));
 </script>
 <template>
-  <div class="flex justify-center">
+  <div class="flex justify-center" v-if="stats">
     <div class="stats shadow">
       <div class="stat">
         <div class="stat-title">Total page views</div>
-        <div class="stat-value">{{ stats?.totalPageviews }}</div>
+        <div class="stat-value">{{ formatNumber(stats.totalPageviews) }}</div>
       </div>
 
       <div class="stat">
         <div class="stat-title">Unique visitors</div>
-        <div class="stat-value">{{ stats?.uniqueVisitors }}</div>
+        <div class="stat-value">{{ formatNumber(stats.uniqueVisitors) }}</div>
       </div>
 
       <div class="stat">
         <div class="stat-title">Views per visitor</div>
-        <div class="stat-value">{{ stats?.viewsPerVisitor }}</div>
+        <div class="stat-value">{{ formatNumber(stats.viewsPerVisitor) }}</div>
       </div>
     </div>
   </div>
